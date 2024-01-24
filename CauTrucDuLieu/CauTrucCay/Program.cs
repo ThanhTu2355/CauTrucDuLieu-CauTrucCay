0033978e160d13d311be818a44a1894579e46a1a
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-01-24 19:33:28 +0700

    code hoan thien cau truc cay

--- CauTrucDuLieu/CauTrucCay/Program.cs
+++ CauTrucDuLieu/CauTrucCay/Program.cs
@@ -10,6 +10,30 @@ namespace CauTrucCay
     {
         static void Main(string[] args)
         {
+            SearchBinaryTree tree = new SearchBinaryTree();
+            tree.TaoCay();
+            Console.WriteLine("Ket qua duyet cay : ");
+            Console.Write("NLR");
+            tree.NLR(tree.root);
+
+            Console.Write("\nLNR");
+            tree.LNR(tree.root);
+
+            Console.Write("\nLRN");
+            tree.LRN(tree.root);
+
+            Console.Write("\nNhap gia tri can tim : ");
+            int x = int.Parse(Console.ReadLine());
+            TreeNode kq = tree.TimKiem(tree.root,x);
+            if(kq == null)
+            {
+                Console.WriteLine($"{x} khong xuat hien trong cay ");
+            }else
+            {
+                Console.WriteLine($"{x} xuat hien trong cay ");
+            }
+
+            Console.ReadLine();
         }
     }
 }
--- CauTrucDuLieu/CauTrucCay/SearchBinaryTree.cs
+++ CauTrucDuLieu/CauTrucCay/SearchBinaryTree.cs
@@ -42,5 +42,44 @@ namespace CauTrucCay
                 Console.Write($"{root.info} ");
             }
         }
+        //them nut vao cay nhi phan tim kiem
+        public void ThemNode(ref TreeNode root, int x)
+        {
+            if(root == null)
+            {
+                TreeNode p = new TreeNode(x);
+                root = p;
+            }else if(root.info>x)
+                ThemNode(ref root.left, x);
+            else if(root.info<x)
+                ThemNode(ref root.right, x);
+        }
+        //phuong thuc tao cay
+        public void TaoCay()
+        {
+            Console.Write("Cho biet so nut trong cay : ");
+            int n = int.Parse(Console.ReadLine());
+            for(int i= 1; i<=n; i++)
+            {
+                Console.Write("Nhap gia tri node thu {0} :",i);
+                int x = int.Parse(Console.ReadLine());
+                ThemNode(ref root, x);
+            }
+        }
+        //tim kiem nut co gia tri x trong cay
+        public TreeNode TimKiem(TreeNode root, int x)
+        {
+            TreeNode kq = null;
+            if (root != null) 
+            {
+                if (root.info == x)
+                    kq = root;
+                else if(root.info>x)
+                    kq=TimKiem(root.left, x);
+                else if (root.info<x)
+                    kq = TimKiem(root.right, x);
+            }
+            return kq;
+        }
     }
 }

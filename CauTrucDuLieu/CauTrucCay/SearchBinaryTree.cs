72e195776d29ed8fd2c652a163fda9cb3fe84710
nguyentu1112005 <nguyentu1112005@gmail.com>
2024-01-24 11:19:16 +0700

    code o truong

---
+++ CauTrucDuLieu/CauTrucCay/SearchBinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauTrucCay
+{
+    class SearchBinaryTree
+    {
+        public TreeNode root;
+
+        public SearchBinaryTree()
+        {
+            root = null;
+        }
+        //duyet cay
+        public void NLR(TreeNode root)
+        {
+            if(root !=null)
+            {
+                Console.Write($"{root.info} ");
+                NLR(root.left);
+                NLR(root.right);
+            }
+        }
+        public void LNR(TreeNode root)
+        {
+            if (root != null)
+            {
+                LNR(root.left);
+                Console.Write($"{root.info} ");
+                LNR(root.right);
+            }
+        }
+        public void LRN(TreeNode root)
+        {
+            if (root != null)
+            {
+                LRN(root.left);
+                LRN(root.right);
+                Console.Write($"{root.info} ");
+            }
+        }
+    }
+}
---
+++ CauTrucDuLieu/CauTrucCay/TreeNode.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CauTrucCay
+{
+    class TreeNode
+    {
+        public int info;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            info = x;
+            left = null;
+            right = null;
+        }
+    }
+}
